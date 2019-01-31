da116a62d7651c1701522fa07e27514fd441df7a
suhyangkim <46915674+suhyangkim@users.noreply.github.com>
2019-01-30 21:37:53 -0800

    SecondFactorial.cpp
    This is another way to compute the factorial utilizing a method.

---
+++ SecondFactorial.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//method declarations
+int factorial(long long int F);
+
+int main(){
+  int N;
+  long long int F;
+
+  //ask the user for a number
+  cout << "Enter a number to use to compute a factorial... or enter a negative number to quit: " << endl;
+  cin >> N;
+  
+  //will continue asking until a negative number of given
+  while(N >= 0){
+    //zero is an exception so it'll print out one
+    if(N == 0){
+      cout << N << "!" << " = " << 1;
+    } else{ //if it's not zero AND it's positive then it will do the factorial
+      F = factorial(F);
+      //after the total number for the factorial is found you print it out  
+      cout << N << "!" << " = " << F << endl << endl;
+      //reasks the user for a number
+      cout << "Enter a number to use to compute a factorial... or enter a negative number to quit: " << endl;
+      cin >> N;
+    }
+  }
+  
+  cout << endl;
+  //if the number input is negative it will print this out
+  cout << "Quitting!" << endl;
+  
+  return 0;
+}
+
+int factorial(long long int F){
+    //reinitializes F to 1.0
+    F = 1.0;
+    //starting from the given number it will decrease by 1 and multiply itself into F 
+    for(int i = N; i > 0; i-=1){
+      F *= i;
+    }
+    return F;
+}
